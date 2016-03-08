495720397ea7c98b2e142a8367dbd1a0a2d8e7b2
Rannix <Trudogolik24@i.ua>
2016-03-08 12:54:35 +0200

    add 1 !

--- Git-Test-Project/src/ua/i/trudogolik24/Main.java
+++ Git-Test-Project/src/ua/i/trudogolik24/Main.java
@@ -3,7 +3,7 @@ package ua.i.trudogolik24;
 public class Main {
 	
 	public static void main(String[] args){
-		System.out.println("Hello from Github!!!");
+		System.out.println("Hello from Github!!!!");
 	}
 
 }
